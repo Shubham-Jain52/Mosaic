d00198433d88c658fa8e657971835f357eaca2c0
Shubham Jain <shubhamjain@Shubhams-Air.bbrouter>
2026-07-26 10:50:12 +0530

    Fix/ adding comments to Sqlite db.

--- core/models.py
+++ core/models.py
@@ -2,21 +2,22 @@
 Core data models for Mosaic.
 """
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
+
 
 @dataclass
 class Review_Comment_Structure:
-    comment_id: int
-    comment_body: str
-    diff_hunk: str     # The code snippet
-    file_path: str     # Which file they commented on
-    author: str
+    comment_id: Optional[int]
+    comment_body: Optional[str]
+    diff_hunk: Optional[str]  # The code snippet
+    file_path: Optional[str]  # Which file they commented on
+    author: Optional[str]
+
+
 @dataclass
 class PR_Structure:
     number: int
     title: str
-    state : str
-    description: str
+    state: str
+    description: Optional[str]
     comments: List[Review_Comment_Structure]
-
-
